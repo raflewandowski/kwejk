c4eb730f18a46d3e0ff288a5c3196537750495f8
Rafal <lewymaster@gmail.com>
2017-11-04 15:23:42 +0100

    Adding Category Model and Controller

---
+++ src/main/java/pl/akademiakodu/kwejk/controller/CategoryController.java
@@ -0,0 +1,7 @@
+package pl.akademiakodu.kwejk.controller;
+
+/**
+ * Created by Rafal Lewandowski on 04.11.2017.
+ */
+public class CategoryController {
+}
---
+++ src/main/java/pl/akademiakodu/kwejk/model/CategoryModel.java
@@ -0,0 +1,7 @@
+package pl.akademiakodu.kwejk.model;
+
+/**
+ * Created by Rafal Lewandowski on 04.11.2017.
+ */
+public class CategoryModel {
+}
